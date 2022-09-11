055359383d84e7c6789aa3416e3f408abff4940b
Vitaliy Zhyryk <80642298+WindyBoss@users.noreply.github.com>
2022-09-11 15:42:57 +0200

    Change BrowserRouter

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ ReactDOM.render(
     <ApiProvider api={contactApi}>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <BrowserRouter>
+          <BrowserRouter basename="/goit-react-hw-08-phonebook/">
             <App />
             <Toaster />
           </BrowserRouter>
