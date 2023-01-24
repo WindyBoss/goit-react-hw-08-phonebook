4cb0a86e7eaf4392c14ffc6e737d6dfb82dbf741
Vitaliy Zhyryk <116021952+Vitaliy-Darigo@users.noreply.github.com>
2023-01-24 20:21:23 +0100

    changed API

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -1,9 +1,9 @@
 /** @format */
 
-import axios from "axios";
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
-/* 
+/*
 ^ For user management is used asyncThunk with axios
 The personal data is based on token - backend internal token with coded user data, which is necessary to add into header authorization - " Bearer ${token}"
 
@@ -16,77 +16,77 @@ log in => .../login
 log out => .../logout
 
 */
-axios.defaults.baseURL = "https://goit-phonebook-api.herokuapp.com";
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
 const token = {
-    set(token) {
-        axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-    },
-    unset() {
-        axios.defaults.headers.common.Authorization = "";
-    },
+  set(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unset() {
+    axios.defaults.headers.common.Authorization = '';
+  },
 };
 
-const register = createAsyncThunk("auth/register", async(credentials) => {
-    return await axios
-        .post("/users/signup", credentials)
-        .then((response) => {
-            token.set(response.data.token);
-            return response.data;
-        })
-        .catch(function(error) {
-            throw error;
-        });
+const register = createAsyncThunk('auth/register', async credentials => {
+  return await axios
+    .post('/users/signup', credentials)
+    .then(response => {
+      token.set(response.data.token);
+      return response.data;
+    })
+    .catch(function (error) {
+      throw error;
+    });
 });
 
-const logIn = createAsyncThunk("auth/logIn", async(credentials) => {
-    return await axios
-        .post("/users/login", credentials)
-        .then((response) => {
-            token.set(response.data.token);
-            return response.data;
-        })
-        .catch(function(error) {
-            throw error;
-        });
+const logIn = createAsyncThunk('auth/logIn', async credentials => {
+  return await axios
+    .post('/users/login', credentials)
+    .then(response => {
+      token.set(response.data.token);
+      return response.data;
+    })
+    .catch(function (error) {
+      throw error;
+    });
 });
 
-const logOut = createAsyncThunk("auth/logout", () => {
-    axios
-        .post("/users/logout")
-        .then(() => {
-            token.unset();
-        })
-        .catch(function(error) {
-            throw error;
-        });
+const logOut = createAsyncThunk('auth/logout', () => {
+  axios
+    .post('/users/logout')
+    .then(() => {
+      token.unset();
+    })
+    .catch(function (error) {
+      throw error;
+    });
 });
 
 const fetchCurrentUser = createAsyncThunk(
-    "auth/refresh",
-    async(_, thunkAPI) => {
-        const state = thunkAPI.getState();
-        const persistedToken = state.auth.token;
+  'auth/refresh',
+  async (_, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const persistedToken = state.auth.token;
 
-        if (persistedToken === null || persistedToken === '') {
-            return thunkAPI.rejectWithValue();
-        }
-        token.set(persistedToken);
-
-        return await axios
-            .get("/users/current")
-            .then((response) => response.data)
-            .catch(function(error) {
-                throw error;
-            });
+    if (persistedToken === null || persistedToken === '') {
+      return thunkAPI.rejectWithValue();
     }
+    token.set(persistedToken);
+
+    return await axios
+      .get('/users/current')
+      .then(response => response.data)
+      .catch(function (error) {
+        throw error;
+      });
+  }
 );
 
 const operations = {
-    register,
-    logIn,
-    logOut,
-    fetchCurrentUser,
+  register,
+  logIn,
+  logOut,
+  fetchCurrentUser,
 };
 
 export default operations;
